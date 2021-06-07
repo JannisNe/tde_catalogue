cae754b8884a3c34b2eb74b726bda44d45206f03
JannisNe <necker@physik.hu-berlin.de>
2021-06-07 23:09:02 +0200

    test compiler error when duplicates are found

--- tests/test_1_test_compiler.py
+++ tests/test_1_test_compiler.py
@@ -1,7 +1,7 @@
 import unittest
 
 from tde_catalogue import main_logger
-from tde_catalogue.compilation.compiler import Compiler
+from tde_catalogue.compilation.compiler import Compiler, CompilerError
 from tde_catalogue.catalogue import Catalogue
 import tde_catalogue.data
 
@@ -11,9 +11,17 @@ logger = main_logger.getChild(__name__)
 
 class TestCompiler(unittest.TestCase):
 
+    def test_duplicate(self):
+        logger.info('\n\n Testing duplicate Error \n')
+        logger.info(f'using {list(Catalogue.registered_catalogues.keys())[0]}')
+        cat_class = list(Catalogue.registered_catalogues.values())[0]
+        catalogues = [cat_class()] * 2
+        with self.assertRaises(CompilerError):
+            Compiler(catalogues)
+
     def test_compilation(self):
         logger.info('\n\n Testing compiler \n')
         logger.info(f'using {list(Catalogue.registered_catalogues.keys())}')
         logger.info('Initialising catalogues')
         catalogues = [cat_class() for cat_class in Catalogue.registered_catalogues.values()]
-        compiler = Compiler(catalogues)
+        Compiler(catalogues)
