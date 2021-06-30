fac20fea80400ac74a401f3dce656eb8c3728195
JannisNe <necker@physik.hu-berlin.de>
2021-06-30 10:51:14 +0200

    number test methods

--- tests/test_1_test_compiler.py
+++ tests/test_1_test_compiler.py
@@ -9,9 +9,10 @@ import tde_catalogue.data
 main_logger.setLevel('DEBUG')
 logger = main_logger.getChild(__name__)
 
+
 class TestCompiler(unittest.TestCase):
 
-    def test_duplicate(self):
+    def test_a_duplicate(self):
         logger.info('\n\n Testing duplicate Error \n')
         logger.info(f'using {list(Catalogue.registered_catalogues.keys())[0]}')
         cat_class = list(Catalogue.registered_catalogues.values())[0]
@@ -19,7 +20,7 @@ class TestCompiler(unittest.TestCase):
         with self.assertRaises(CompilerError):
             Compiler(catalogues)
 
-    def test_compilation(self):
+    def test_b_compilation(self):
         logger.info('\n\n Testing compiler \n')
         logger.info(f'using {list(Catalogue.registered_catalogues.keys())}')
         logger.info('Initialising catalogues')
