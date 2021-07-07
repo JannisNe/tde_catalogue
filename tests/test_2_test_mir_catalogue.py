169374649c39857a76aa2e8b9b1064be93c46728
JannisNe <necker@physik.hu-berlin.de>
2021-07-07 20:01:15 +0200

    add test of WISE access

--- tests/test_2_test_mir_catalogue.py
+++ tests/test_2_test_mir_catalogue.py
@@ -1,23 +1,31 @@
-import unittest, os, shutil
+import unittest, os, shutil, copy
+import numpy as np
 
 from tde_catalogue import main_logger, cache_dir, plots_dir
+from tde_catalogue.utils.mirong_sample import get_mirong_sample
 from tde_catalogue.data.mir_flares.panstarrs_parent_sample import PanstarrsParentSample
+from tde_catalogue.data.mir_flares.wise_data import WISEData
 
 
+main_logger.setLevel('DEBUG')
 logger = main_logger.getChild(__name__)
 
-test_ra = 0
-test_dec = 0
+
+mirong_sample = get_mirong_sample()
+mirong_test_id = 0
+
+test_ra = mirong_sample['RA'].iloc[mirong_test_id]
+test_dec = mirong_sample['DEC'].iloc[mirong_test_id]
 test_radius_arcsec = 3600
 
 
-class TestPanstarrsParentSample(PanstarrsParentSample):
+class PanstarrsParentSampleTestVersion(PanstarrsParentSample):
     """
     Does the same as PanstarrsParentSample but
     only on a confined region in the sky to deal with small tables
     """
 
-    base_name = PanstarrsParentSample.base_name + '_test'
+    base_name = 'test/' + PanstarrsParentSample.base_name
     MAST_table_name = PanstarrsParentSample.MAST_table_name + '_test'
     query = f"""
     SELECT
@@ -32,9 +40,9 @@ class TestPanstarrsParentSample(PanstarrsParentSample):
     """
 
     def __init__(self):
-        super().__init__(TestPanstarrsParentSample.base_name,
-                         TestPanstarrsParentSample.MAST_table_name,
-                         TestPanstarrsParentSample.query)
+        super().__init__(PanstarrsParentSampleTestVersion.base_name,
+                         PanstarrsParentSampleTestVersion.MAST_table_name,
+                         PanstarrsParentSampleTestVersion.query)
 
     def clean_up(self):
         logger.info(f'removing {cache_dir}')
@@ -43,21 +51,75 @@ class TestPanstarrsParentSample(PanstarrsParentSample):
         self.mastcasjob.drop_table(self.MAST_table_name)
 
 
-class TestCompiler(unittest.TestCase):
+class WISEDataTestVersion(WISEData):
+    """
+    Same as WISEData but only for one confined region of the sky
+    """
 
-    def test_a_query_and_download(self):
+    base_name = 'test/' + WISEData.base_name
+    where = f"""
+        WHERE
+            CONTAINS(POINT('ICRS',ra, dec), CIRCLE('ICRS',{test_ra},{test_dec},{test_radius_arcsec/3600}))=1"""
+
+    def __init__(self):
+        super().__init__(n_chunks=3,
+                         where=WISEDataTestVersion.where,
+                         base_name=WISEDataTestVersion.base_name,
+                         parent_sample=PanstarrsParentSampleTestVersion)
+        self._cached_tap_output = None
+
+    def get_tap_output(self, chunk_number, table_name):
+        dec_intervall = self.dec_intervalls[chunk_number]
+        logger.info(f'getting TAP output for DEC interval {dec_intervall}')
+
+        if isinstance(self._cached_tap_output, type(None)):
+            logger.debug('No cached TAP output')
+
+            queue = f"""
+            SELECT
+                source_id, ra, dec, sigra, sigdec, nb, na, cc_flags
+            FROM
+                {table_name}
+            WHERE
+                CONTAINS(POINT('ICRS', ra, dec), CIRCLE('ICRS',{test_ra},{test_dec},{test_radius_arcsec/3600}))
+            """
+
+            logger.info(f"Queue: {queue}")
+            query_job = WISEData.service.submit_job(queue)
+            query_job.run()
+            logger.info(f'Job: {query_job.url}; {query_job.phase}')
+            logger.info('waiting ...')
+            query_job.wait()
+            logger.info('Done!')
+
+            self._cached_tap_output = query_job.fetch_result().to_table().to_pandas()
+
+        m = (self._cached_tap_output.nb < 2) & (self._cached_tap_output.na < 1)
+        dec_m = (self._cached_tap_output.dec > min(dec_intervall)) & (self._cached_tap_output.dec < max(dec_intervall))
+        cc_m = np.array([cc.startswith('00') for cc in self._cached_tap_output.cc_flags])
+        tap_res = copy.copy(self._cached_tap_output[m & cc_m & dec_m])
+        logger.debug(f'Found {len(tap_res)} objects.')
+
+        return tap_res
+
+
+class TestMIRFlareCatalogue(unittest.TestCase):
+
+    def test_a_test_panstarrs_parent_sample(self):
         logger.info('\n\n Testing PanstarrsParentSample \n')
         logger.info('querying MAST and downloading table')
-        TestPanstarrsParentSample()
-
-    def test_b_plots(self):
+        pps = PanstarrsParentSampleTestVersion()
         logger.info('\n testing plots \n')
-        pps = TestPanstarrsParentSample()
         pps.plot_skymap()
         pps.plot_cutout(0)
 
+    def test_b_test_wise_data(self):
+        logger.info('\n\n Testing WISE Data \n')
+        wise_data = WISEDataTestVersion()
+        wise_data.match_all_chunks('allwise_p3as_psd')
+
     @classmethod
     def tearDownClass(cls):
-        logger.info('clean up')
-        pps = TestPanstarrsParentSample()
+        logger.info('\n clean up \n')
+        pps = PanstarrsParentSampleTestVersion()
         pps.clean_up()
