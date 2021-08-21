3fdb59872a72a48090660710bd8fa74acfec9102
JannisNe <necker@physik.hu-berlin.de>
2021-08-21 14:46:45 +0200

    add complete lightcurve code

--- tde_catalogue/data/mir_flares/wise_data.py
+++ tde_catalogue/data/mir_flares/wise_data.py
@@ -1,11 +1,11 @@
-import os, subprocess, copy
+import os, subprocess, copy, json, argparse
 import pandas as pd
 import numpy as np
 import pyvo as vo
 import astropy.units as u
 from astropy.table import Table
 
-from tde_catalogue import main_logger, cache_dir, plots_dir
+from tde_catalogue import main_logger, cache_dir, plots_dir, output_dir
 from tde_catalogue.data.mir_flares import base_name as mir_base_name
 from tde_catalogue.data.mir_flares.panstarrs_parent_sample import PanstarrsParentSample
 
@@ -42,13 +42,18 @@ class WISEData:
     }
 
     constraints = [
-        "nb<2",
-        "na<1",
+        "nb < 2",
+        "na < 1",
         "cc_flags like '00%'",
         "qi_fact >= 1",
         "saa_sep >= 5",
         "moon_masked = '00'"
     ]
+
+    bands = ['W1', 'W2']
+    flux_key_ext = "_flux"
+    error_key_ext = "_flux_error"
+    band_plot_colors = {'W1': 'r', 'W2': 'b'}
 
     def __init__(self,
                  min_sep_arcsec=60,
@@ -63,6 +68,10 @@ class WISEData:
         :param parent_sample_class: object, class for parent sample
         """
 
+        #######################################################################################
+        # START SET-UP          #
+        #########################
+
         parent_sample = parent_sample_class()
         self.base_name = base_name
         self.min_sep = min_sep_arcsec * u.arcsec
@@ -75,11 +84,22 @@ class WISEData:
 
         # set up directories
         self.cache_dir = os.path.join(cache_dir, base_name)
+        self.output_dir = os.path.join(output_dir, base_name)
+        self.lightcurve_dir = os.path.join(self.output_dir, "lightcurves")
         self.plots_dir = os.path.join(plots_dir, base_name)
 
-        for d in [self.cache_dir, self.plots_dir]:
+        for d in [self.cache_dir, self.output_dir, self.lightcurve_dir, self.plots_dir]:
             if not os.path.isdir(d):
                 os.makedirs(d)
+
+        # set up result attributes
+        self._all_lightcurves = None
+        self.binned_lightcurves = None
+        self._cached_binned_lcs_json = os.path.join(self.cache_dir, "binned_lcs.json")
+
+        #########################
+        # END SET-UP            #
+        #######################################################################################
 
         self.parent_sample = parent_sample
         min_dec = np.floor(min(self.parent_sample.df[self.parent_dec_key]))
@@ -110,6 +130,10 @@ class WISEData:
             logger.debug(f"{table_name} not in Table. Assuming it is the right name already.")
         return table_name
 
+    ###########################################################################################################
+    # START MATCH PARENT SAMPLE TO WISE SOURCES         #
+    #####################################################
+
     def match_all_chunks(self, **table_name):
         for i in range(len(self.dec_intervalls)):
             self.match_single_chunk(i, **table_name)
@@ -139,11 +163,11 @@ class WISEData:
         # write to IPAC formatted table
         _selected_parent_sample_astrotab = Table.from_pandas(selected_parent_sample)
         _parent_sample_declination_band_file = os.path.join(self.cache_dir, f"parent_sample_chunk{chunk_number}.xml")
-        logger.debug(f"writing {len(_selected_parent_sample_astrotab)} objects to {_parent_sample_declination_band_file}")
-        _selected_parent_sample_astrotab.write(_parent_sample_declination_band_file, format='ipac')
+        logger.debug(f"writing {len(_selected_parent_sample_astrotab)} "
+                     f"objects to {_parent_sample_declination_band_file}")
+        _selected_parent_sample_astrotab.write(_parent_sample_declination_band_file, format='ipac', overwrite=True)
 
         # use Gator to query IRSA
-        # TODO: use the full_cat_select input!
         _output_file = os.path.join(self.cache_dir, f"wise_data_chunk{chunk_number}.tbl")
         submit_cmd = f'curl ' \
                      f'-o {_output_file} ' \
@@ -179,37 +203,13 @@ class WISEData:
             self.parent_wise_source_id_key
         ] = list(gator_res["cntr"])
 
-    def get_photometry_query_string(self, table_name):
-        """
-        Construct a query string to submit to IRSA
-        :param table_name: str, table name
-        :return: str
-        """
-        logger.debug(f"constructing query for {table_name}")
-        db_name = self.get_db_name(table_name)
-        nice_name = self.get_db_name(table_name, nice=True)
+    ###################################################
+    # END MATCH PARENT SAMPLE TO WISE SOURCES         #
+    ###########################################################################################################
 
-        flux_keys = list(self.photometry_table_keymap[nice_name].keys())
-        keys = ['mjd'] + flux_keys
-        id_key = ['cntr_mf'] if 'allwise' in db_name else ['allwise_cntr']
-
-        q = 'SELECT' \
-            '   '
-        for k in keys:
-            q += f' {db_name}.{k}'
-
-        q += ' mine.wise_id'
-        q += f'FROM {db_name}'
-        q += f'INNER JOIN TAP_UPLOAD.pois AS mine ON {db_name}.{id_key} = mine.WISE_id'
-        q += 'WHERE'
-
-        for c in self.constraints:
-            q += f'{db_name}.{c} and'
-
-        q.strip(" and")
-
-        logger.debug(f"\n{q}")
-        return q
+    ###########################################################################################################
+    # START GET PHOTOMETRY DATA       #
+    ###################################
 
     def get_photometric_data(self, tables=None):
         """
@@ -224,14 +224,53 @@ class WISEData:
                 'AllWISE Multiepoch Photometry Table'
             ]
 
-        jobs = dict()
+        self._query_for_photometry(tables)
+        self._select_individual_lightcurves_and_bin()
+        self._cache_binned_lightcurves()
 
-        for t in tables:
-            qstring = self.get_photometry_query_string(t)
-            job = WISEData.service.submit_job(qstring)
+    def _get_photometry_query_string(self, table_name):
+        """
+        Construct a query string to submit to IRSA
+        :param table_name: str, table name
+        :return: str
+        """
+        logger.debug(f"constructing query for {table_name}")
+        db_name = self.get_db_name(table_name)
+        nice_name = self.get_db_name(table_name, nice=True)
+        flux_keys = list(self.photometry_table_keymap[nice_name].keys())
+        keys = ['mjd'] + flux_keys
+        id_key = 'cntr_mf' if 'allwise' in db_name else 'allwise_cntr'
+
+        q = 'SELECT \n\t'
+        for k in keys:
+            q += f'{db_name}.{k}, '
+        q += '\n\tmine.wise_id \n'
+        q += f'FROM\n\t{db_name} \n'
+        q += f'INNER JOIN\n\tTAP_UPLOAD.ids AS mine ON {db_name}.{id_key} = mine.WISE_id \n'
+        q += 'WHERE \n'
+        for c in self.constraints:
+            q += f'\t{db_name}.{c} and \n'
+        q = q.strip(" and \n")
+        logger.debug(f"\n{q}")
+        return q
+
+    def _query_for_photometry(self, tables):
+
+        # only integers can be uploaded
+        wise_id = [int(idd) for idd in self.parent_sample.df[self.parent_wise_source_id_key]]
+        upload_table = Table({'wise_id': wise_id})
+
+        # ----------------------------------------------------------------------
+        #     Do the query
+        # ----------------------------------------------------------------------
+
+        jobs = dict()
+        for t in np.atleast_1d(tables):
+            qstring = self._get_photometry_query_string(t)
+            job = WISEData.service.submit_job(qstring, uploads={'ids': upload_table})
             job.run()
             logger.info(f'submitted job for {t}: ')
-            logger.info(f'Job: {job.url}; {job.phase}')
+            logger.debug(f'Job: {job.url}; {job.phase}')
             jobs[t] = job
 
         lightcurves = list()
@@ -244,5 +283,74 @@ class WISEData:
             lightcurve = job.fetch_result().to_table().to_pandas()
             lightcurves.append(lightcurve.rename(columns=self.photometry_table_keymap[t]))
 
-        combined_lightcurve = pd.concat(lightcurves)
-        return combined_lightcurve
+        self._all_lightcurves = pd.concat(lightcurves)
+
+    def _select_individual_lightcurves_and_bin(self, drop=True):
+
+        # ----------------------------------------------------------------------
+        #     select individual lightcurves
+        # ----------------------------------------------------------------------
+
+        logger.info('selecting individual lightcurves and bin ...')
+        self.binned_lightcurves = dict()
+        for ID in self._all_lightcurves.wise_id.unique():
+            m = self._all_lightcurves.wise_id == ID
+            lightcurve = self._all_lightcurves[m]
+            if drop:
+                # drop the selected rows from self._all_lightcurves to save memory
+                self._all_lightcurves.drop(self._all_lightcurves.index[m], inplace=True)
+
+        # ----------------------------------------------------------------------
+        #     bin lightcurves
+        # ----------------------------------------------------------------------
+
+            # bin lightcurves in time intervals where observations are closer than 100 days together
+            sorted_mjds = np.sort(lightcurve.mjd)
+            epoch_bounds_mask = (sorted_mjds[1:] - sorted_mjds[:-1]) > 100
+            epoch_bounds = np.array(
+                [lightcurve.mjd.min()] +
+                list(sorted_mjds[1:][epoch_bounds_mask]) +
+                [lightcurve.mjd.max()*1.01]  # this just makes sure that the last datapoint gets selected as well
+            )
+            epoch_intervals = np.array([epoch_bounds[:-1], epoch_bounds[1:]]).T
+
+            binned_lc = pd.DataFrame()
+            for ei in epoch_intervals:
+                r = dict()
+                epoch_mask = (lightcurve.mjd >= ei[0]) & (lightcurve.mjd < ei[1])
+                r['mean_mjd'] = np.median(lightcurve.mjd[epoch_mask])
+
+                for b in self.bands:
+                    f = lightcurve[f"{b}{self.flux_key_ext}"][epoch_mask]
+                    mean = np.average(f, weights=lightcurve[f"{b}{self.error_key_ext}"][epoch_mask])
+                    u = np.sqrt(sum((f - mean) ** 2) / len(f))
+                    r[f'{b}_mean_flux'] = mean
+                    r[f'{b}_flux_rms'] = u
+
+                binned_lc = binned_lc.append(r, ignore_index=True)
+
+            self.binned_lightcurves[int(ID)] = binned_lc.to_dict()
+
+    def _cache_binned_lightcurves(self):
+        # ----------------------------------------------------------------------
+        #     save to JSON
+        # ----------------------------------------------------------------------
+        logger.debug(f"saving to {self._cached_binned_lcs_json}")
+        with open(self._cached_binned_lcs_json, "w") as f:
+            json.dump(self.binned_lightcurves, f)
+
+    #################################
+    # END GET PHOTOMETRY DATA       #
+    ###########################################################################################################
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-l', '--logging_level', type=str, default='INFO')
+    cfg = parser.parse_args()
+
+    main_logger.setLevel(cfg.logging_level)
+
+    wise_data = WISEData()
+    wise_data.match_all_chunks()
+    wise_data.get_photometric_data()
