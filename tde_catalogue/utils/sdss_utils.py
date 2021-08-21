ff5ac5b504431c7388c0a92c1cd447eeb981e40f
JannisNe <necker@physik.hu-berlin.de>
2021-08-21 14:47:44 +0200

    add sdss utils

---
+++ tde_catalogue/utils/sdss_utils.py
@@ -0,0 +1,32 @@
+import getpass, os
+from SciServer import SkyServer
+import matplotlib.pyplot as plt
+
+
+def get_sdss_credentials():
+    if not os.environ.get('SDSS_USERID'):
+        os.environ['SDSS_USERID'] = input('Enter SDSS user ID:')
+    if not os.environ.get('SDSS_USERPW'):
+        os.environ['SDSS_USERPW'] = getpass.getpass('Enter SDSS password:')
+    return os.environ['SDSS_USERID'], os.environ['SDSS_USERPW']
+
+
+def plot_cutout(ra, dec, arcsec=20, arcsec_per_px=1, interactive=False, fn=None):
+    ang_px = int(arcsec / arcsec_per_px)
+    ang_deg = arcsec / 3600
+
+    im = SkyServer.getJpegImgCutout(ra, dec, scale=arcsec_per_px, height=ang_px, width=ang_px)
+
+    fig, ax = plt.subplots()
+    ax.imshow(im, origin='upper',
+              extent=([ra + ang_deg / 2, ra - ang_deg / 2,
+                       dec - ang_deg / 2, dec + ang_deg / 2]),
+              cmap='gray')
+
+    ax.scatter(ra, dec, marker='x', color='red')
+
+    if interactive:
+        return fig, ax
+    else:
+        fig.savefig(fn)
+        plt.close()
