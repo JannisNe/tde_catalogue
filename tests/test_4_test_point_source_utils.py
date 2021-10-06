2d452aa4a890dc0876e3ed4bbecd507ea1e6d2d9
JannisNe <necker@physik.hu-berlin.de>
2021-10-06 10:11:30 +0200

    cleanup

--- tests/test_4_test_point_source_utils.py
+++ tests/test_4_test_point_source_utils.py
@@ -1,14 +1,7 @@
-import unittest, shutil, argparse, time, os
-import numpy as np
-from astropy.coordinates import SkyCoord
-from astropy import units as u
+import unittest
 
-from tde_catalogue import main_logger, cache_dir
-from tde_catalogue.utils.mirong_sample import get_mirong_sample
-from tde_catalogue.data.mir_flares.panstarrs_parent_sample import PanstarrsParentSample
+from tde_catalogue import main_logger
 from tde_catalogue.data.mir_flares.wise_data import WISEData
-from tde_catalogue.data.mir_flares.sdss_parnet_sample import SDSSParentSample, CasJobs
-from tde_catalogue.data.mir_flares.combined_parent_sample import CombinedParentSample
 from tde_catalogue.utils.point_source_utils import get_point_source_wise_data
 
 
