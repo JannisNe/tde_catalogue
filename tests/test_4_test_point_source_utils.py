0abffc0ded21753876c3eae7487f53a2ef618bc6
JannisNe <necker@physik.hu-berlin.de>
2022-01-28 18:52:33 +0100

    use timewise

--- tests/test_4_test_point_source_utils.py
+++ tests/test_4_test_point_source_utils.py
@@ -1,16 +1,15 @@
-import unittest
+import unittest, logging
 
 from tde_catalogue import main_logger
-from tde_catalogue.data.mir_flares.wise_data import WISEData
+from tde_catalogue.data.mir_flares import base_name as mir_base_name
 from tde_catalogue.utils.point_source_utils import get_point_source_wise_data
 
-
 logger = main_logger.getChild(__name__)
-
+logging.getLogger('timewise').setLevel('INFO')
 
 test_ra = 243.2494163
 test_dec = 42.3277313
-base_name = 'test/' + WISEData.base_name + '_point_source_utils'
+base_name = f'test/{mir_base_name}/WISEData/_point_source_utils'
 
 
 class TestPointSourceUtils(unittest.TestCase):
