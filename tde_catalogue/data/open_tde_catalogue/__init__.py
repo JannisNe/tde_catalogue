7ac7eed29e9131a4f5f7b85618aa974c05e945e3
JannisNe <necker@physik.hu-berlin.de>
2022-03-18 16:13:35 +0100

    update open tde catalogue stuff

--- tde_catalogue/data/open_tde_catalogue/__init__.py
+++ tde_catalogue/data/open_tde_catalogue/__init__.py
@@ -15,7 +15,16 @@ raw_data = f'{os.path.dirname(__file__)}/TheOpenTDECatalog.csv'
 class OpenTDECatalogue(Catalogue):
 
     def __init__(self):
-        super().__init__(pd.read_csv(raw_data), f'OpenTDECatalogue')
+        df = pd.read_csv(raw_data)
+
+        # exclude TNS sources, that are weird in tde.space
+        m = df.Name == "AT"
+        m2 = df.Name == "ZTF18abxftqm"
+        df = df[~m & (~m2)]
+
+        # exclude sources without date
+
+        super().__init__(df, f'OpenTDECatalogue')
 
     @staticmethod
     def reformat_catalogue(unformated_catalogue):
@@ -31,10 +40,24 @@ class OpenTDECatalogue(Catalogue):
         logger.warning('No field RA and DEC!')
         reformatted_catalogue = pd.DataFrame(columns=Catalogue.columns)
 
+        date = list()
+        for x in unformated_catalogue[key_map['Discovery Date (UT)']]:
+            if isinstance(x, str):
+                date.append(x.replace('/', '-'))
+            else:
+                date.append(x)
+
+        reformatted_catalogue['Discovery Date (UT)'] = date
+
         for k in Catalogue.columns:
+            if k == 'Discovery Date (UT)':
+                continue
+
             if k in key_map:
                 reformatted_catalogue[k] = unformated_catalogue[key_map[k]]
             else:
                 reformatted_catalogue[k] = [np.nan] * len(unformated_catalogue)
+
+        reformatted_catalogue["Ext. catalog/s"] = "OpenTDECatalogue"
 
         return reformatted_catalogue
